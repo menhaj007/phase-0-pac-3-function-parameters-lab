99bc6c934f47ea91124afb3d2e9bc239e4cce08e
menhaj <menhaj007@yahoo.com>
2021-03-07 18:20:31 -0500

    Complete assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+// function logTwoValues(value1, value2) {
+//     console.log(`The two values are ${value1} and ${value2}`);
+// }
+
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}!`);
+// }
+
+// function sayHelloTo(firstName = "User") {
+//     console.log(`Hello, ${firstName}`);
+// }
+// function introductionWithLanguageOptional(name, language = "JavaScript") {
+//     console.log(`Hello, ${name} and ${language}`);
+// }
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language  = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
